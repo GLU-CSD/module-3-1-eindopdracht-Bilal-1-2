e98023853b7d91a1b92a67f246c9f3e612daea5d
Bilal-1-2 <bilal127g14@gmail.com>
2025-03-05 12:13:19 +0100

    11

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-function selectScore(element) {
-    // Remove "selected" class from all divs
-    document.querySelectorAll(".nutri-score div").forEach(div => div.classList.remove("selected"));
 
-    // Add "selected" class to the clicked div
-    element.classList.add("selected");
-}
+  document.addEventListener("DOMContentLoaded", function () {
+      // Selecteer alle "Add to cart" knoppen
+      let buttons = document.querySelectorAll(".add-to-cart");
+
+      buttons.forEach(button => {
+          button.addEventListener("click", function () {
+              let product = this.getAttribute("data-product"); 
+              let price = this.getAttribute("data-price");
+
+              // Haal bestaande items op of maak een lege array
+              let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+              // Voeg nieuw product toe
+              cart.push({ product, price });
+
+              // Opslaan in LocalStorage
+              localStorage.setItem("cart", JSON.stringify(cart));
+
+              alert(product + " is toegevoegd aan je bestelling!");
+          });
+      });
+  });
+
